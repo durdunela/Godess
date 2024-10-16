8ddc7befe465d4af7dc2cc95a06c3a07e3399161
durdunela <durdunela@gmail.com>
2024-10-16 15:07:07 +0400

    changes

--- godess/lib/presentation/views/main_view.dart
+++ godess/lib/presentation/views/main_view.dart
@@ -10,7 +10,10 @@ import 'package:godess/presentation/views/prayers_view.dart';
 import 'package:godess/presentation/views/television_view.dart';
 
 class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _MainPageState createState() => _MainPageState();
 }
 
